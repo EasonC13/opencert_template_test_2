85159d36f424a0dbdf0bc5091f8a39ac1e4293f4
EasonC13 <gtes9950406@gmail.com>
2021-02-02 18:19:21 +0800

    add background image

--- src/templates/customTemplate/customTemplate.tsx
+++ src/templates/customTemplate/customTemplate.tsx
@@ -11,14 +11,32 @@ const style = css`
   }
 `;
 
+
+const containerStyle = css`
+  background-image: url('https://i.imgur.com/EjypxQN.png');
+  //background-image: url('https://i.imgur.com/iOjXS2p.png');
+  //background-color: #324353;
+  color: black;
+  padding: 15px;
+  margin: auto;
+  width: 80%;
+  text-align: center;
+  height: 295;
+  width: 195;
+`;
+
+const titleCss = css`
+  color: rgb(175, 149, 58)
+`
+
 export const CustomTemplate: FunctionComponent<TemplateProps<CustomTemplateCertificate> & { className?: string }> = ({
   document,
   className = ""
 }) => {
   return (
-    <div css={style} className={className} id="custom-template">
+    <div css={containerStyle} className={className} id="custom-template">
       <div>
-        <h1>{document?.foo?.title ?? "Default title"}</h1>
+        <h1>{document?.foo?.title ??  "Default title"}</h1>
         <h3>Award to</h3>
         <h2>{document.name}</h2>
         <br></br>
